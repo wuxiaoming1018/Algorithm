fb77d7763417f395933bf82d9b85b3dd064b619f
Administrator <1398565099@qq.com>
2017-12-06 11:06:18 +0800

    冒泡排序(适合3-5个数字的排序)

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -10,15 +10,15 @@ package sort;
  */
 public class BubbleSort {
 
-    private void sort(int[] data){
+    private void sort(int[] data) {
         System.out.println("\n排序后：");
-        for(int j=data.length-1;j>0;j--){
+        for (int j = data.length - 1; j > 0; j--) {
             boolean flag = true;
             for (int i = 0; i < j; i++) {
-                if(data[i]>data[i+1]){
+                if (data[i] > data[i + 1]) {
                     int temp = data[i];
-                    data[i] = data[i+1];
-                    data[i+1] = temp;
+                    data[i] = data[i + 1];
+                    data[i + 1] = temp;
                     flag = false;
                 }
             }
@@ -26,18 +26,17 @@ public class BubbleSort {
                 break;
             }
         }
-
         for (int datum : data) {
-            System.out.print(datum+" ");
+            System.out.print(datum + " ");
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         BubbleSort bubbleSort = new BubbleSort();
-        int[] data = new int[]{4,25,41,15,3,5,14,9,8,62,29};
+        int[] data = new int[]{4, 25, 41, 15, 3, 5, 14, 9, 8, 62, 29};
         System.out.println("排序前：");
         for (int datum : data) {
-            System.out.print(datum+" ");
+            System.out.print(datum + " ");
         }
         bubbleSort.sort(data);
     }
