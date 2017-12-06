b63f77b2f91a9e51280919d14eadbf29b0091c4b
Administrator <1398565099@qq.com>
2017-12-06 17:44:55 +0800

    多关键字排序

--- src/sort/ManyKeySort.java
+++ src/sort/ManyKeySort.java
@@ -49,7 +49,7 @@ public class ManyKeySort {
      * @param array
      */
     private static void bubbleSort(Card[] array){
-        for (int i = array.length - 1; i >= 0; i--) {
+        for (int i = array.length - 1; i > 0; i--) {
             boolean flag = true;
             for (int j = 0; j < i; j++) {
                 if(array[j].compareTo(array[j+1])>0){
@@ -69,13 +69,19 @@ public class ManyKeySort {
     }
 
     public static void main(String[] args){
-        Card[] array = new Card[]{
+        /*Card[] array = new Card[]{
                 new Card(3,9),
                 new Card(2,9),
                 new Card(4,2),
                 new Card(4,3),
                 new Card(1,3),
                 new Card(3,3)
+        };*/
+        Card[] array = new Card[]{
+                new Card(3,9),
+                new Card(2,7),
+                new Card(1,7),
+                new Card(4,3)
         };
         bubbleSort(array);
     }
